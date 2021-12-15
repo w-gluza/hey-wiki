02bf6f0d03a7abdebcdea5a6e5dca1ac211cc980
Wioletta Gluza <wiolagluza@gmail.com>
2021-12-15 18:12:23 +0100

    update favicon, fix console warning

--- client/src/components/bot-suggestions/BotSuggestions.jsx
+++ client/src/components/bot-suggestions/BotSuggestions.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const BotSuggestions = ({ suggestions, userMessage, sendMessage }) => {
-  console.log('array of suggestions', suggestions);
   return (
     <div className="suggestions-container">
       {suggestions.map((suggestion) => (
         <button
+          key={suggestion.label}
           className="btn-suggestions"
           onClick={() => {
             userMessage(suggestion.label);
