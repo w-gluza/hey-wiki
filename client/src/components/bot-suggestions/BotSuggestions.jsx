7a48a52890076b88ca89a47f688a70eba39d1379
Wioletta Gluza <wiolagluza@gmail.com>
2021-12-17 22:35:52 +0100

    add suggestion title

--- client/src/components/bot-suggestions/BotSuggestions.jsx
+++ client/src/components/bot-suggestions/BotSuggestions.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import BootSingle from '../bot-single/BotSingle';
 
 const BotSuggestions = ({ suggestions, userMessage, sendMessage }) => {
   return (
-    <div className="suggestions-container">
-      {suggestions.map((suggestion) => (
-        <button
-          key={suggestion.label}
-          className="btn-suggestions"
-          onClick={() => {
-            userMessage(suggestion.label);
-            sendMessage(suggestion.label);
-          }}
-        >
-          {suggestion.label}
-        </button>
-      ))}
-    </div>
+    <>
+      <BootSingle msg={"Did you mean:"} />
+      <div className="suggestions-container">
+        {suggestions.map((suggestion) => (
+          <button
+            key={suggestion.label}
+            className="btn-suggestions"
+            onClick={() => {
+              userMessage(suggestion.label);
+              sendMessage(suggestion.label);
+            }}
+          >
+            {suggestion.label}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
 export default BotSuggestions;
