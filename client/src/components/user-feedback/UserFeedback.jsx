cf0b0e4c865c745d06a6f0a7f89b52ba2aa25724
Wioletta Gluza <wiolagluza@gmail.com>
2021-12-18 19:33:55 +0100

    user feedbak icons ui and placemant

--- client/src/components/user-feedback/UserFeedback.jsx
+++ client/src/components/user-feedback/UserFeedback.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Icon from '../../assets/icons';
+import Icon from "../../assets/icons";
 import BootSingle from "../bot-single/BotSingle";
 
-const UserFeedback = ({
-  labels,
-  sendMessage,
-  textMessages,
-}) => {
+const UserFeedback = ({ labels, sendMessage, textMessages }) => {
   // Check if a variable is not blank
   function isBlank(str) {
     return !str || /^\s*$/.test(str);
@@ -28,21 +24,25 @@ const UserFeedback = ({
         textMessages.map((singleMsg) => (
           <BootSingle key={singleMsg.text} msg={singleMsg.text} />
         ))}
-      {filteredOptions &&
-        filteredOptions.map((option) => (
-          <button
-            type="button"
-            className={option === "YES" ? "icon-positive" : "icon-negative"}
-            key={option}
-            onClick={() => sendMsg(option)}
-          >
-            {option === "YES" ? (
-              <Icon name="Positive" alt="Yes" />
-            ) : (
-              <Icon name="Positive" alt="No" />
-            )}
-          </button>
-        ))}
+      <div className="feedback-icons-container">
+        {filteredOptions &&
+          filteredOptions.map((option) => (
+            <span className="icon-wrapper">
+              <button
+                type="button"
+                className={option === "YES" ? "icon-positive" : "icon-negative"}
+                key={option}
+                onClick={() => sendMsg(option)}
+              >
+                {option === "YES" ? (
+                  <Icon name="Positive" alt="Yes" />
+                ) : (
+                  <Icon name="Positive" alt="No" />
+                )}
+              </button>
+            </span>
+          ))}
+      </div>
     </div>
   );
 };
